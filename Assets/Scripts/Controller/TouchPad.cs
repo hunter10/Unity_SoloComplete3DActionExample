68f29e65523cc4893dbb697c73da96c439990eb8
hunter Mac <hunter10@naver.com>
2017-08-14 17:53:02 +0900

    리소스 최초 커밋

---
+++ Assets/Scripts/Controller/TouchPad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TouchPad : MonoBehaviour {
+
+    private RectTransform _touchPad;
+
+    // 터치 입력 중에 방향 컨트롤러의 영역 안에 있는 입력을 구분하기 위한 아이디어입니다. 
+    private int _touchId = -1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerMovement : MonoBehaviour {
+
+    protected Animator avatar;
+
+    float lastAttackTime, lastSkillTime, lastDashTime;
+    public bool attacking = false;
+    public bool dashing = false;
+
+    void Start () {
+        avatar = GetComponent<Animator>();	
+	}
+
+    float h, v;
+
+    public void OnStickChanged(Vector2 stickPos)
+    {
+        h = stickPos.x;
+        v = stickPos.y;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if(avatar)
+        {
+            float back = 1f;
+            if (v < 0f) back = -1f;
+            avatar.SetFloat("Speed", (h*h + v*v));
+
+            Rigidbody rigibody = GetComponent<Rigidbody>();
+
+            if(rigibody)
+            {
+                Vector3 speed = rigibody.velocity;
+                speed.x = 4 * h;
+                speed.z = 4 * v;
+                rigibody.velocity = speed;
+                if (h != 0f && v != 0f){
+                    transform.rotation = Quaternion.LookRotation(new Vector3(h, 0f, v));
+                }
+            }
+        }
+	}
+
+    public void OnAttackDown()
+    {
+        attacking = true;
+        avatar.SetBool("Combo", true);
+        StartCoroutine(StartAttack());
+    }
+
+    public void OnAttackUp()
+    {
+        avatar.SetBool("Combo", false);
+        attacking = false;
+    }
+
+    IEnumerator StartAttack()
+    {
+        if (Time.time - lastAttackTime > 1f){
+            lastAttackTime = Time.time;
+            while(attacking){
+                avatar.SetTrigger("AttackStart");
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+}
