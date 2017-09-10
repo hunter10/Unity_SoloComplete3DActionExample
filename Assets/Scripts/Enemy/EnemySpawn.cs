daf3ad8f7d0b5fbc03f365e323d867237ac482be
hunter Mac <hunter10@naver.com>
2017-09-10 21:10:10 +0900

    적생성

---
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject enemy;
+    public float intervalTime = 10f;
+    public Transform[] spawnPools;
+
+	void Start () {
+        InvokeRepeating("Spawn", intervalTime, intervalTime);	
+	}
+	
+    void Spawn()
+    {
+        int spawnPoolIndex = Random.Range(0, spawnPools.Length);
+
+        Instantiate(enemy, spawnPools[spawnPoolIndex].position, spawnPools[spawnPoolIndex].rotation);
+
+    }
+	
+}
